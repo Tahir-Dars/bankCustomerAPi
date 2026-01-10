6eefa2194a13d7544ca5cadb1c49a8c6d573b527
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-01-10 18:46:55 +0500

    Add CustomerController with CRUD endpoints for Customer entity

---
+++ src/main/java/com/bank/controller/customerController.java
@@ -0,0 +1,59 @@
+package com.bank.controller;
+
+import com.bank.model.Customer;
+import com.bank.service.CustomerServiceLogic;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+
+public class customerController {
+
+    private final CustomerServiceLogic customerService;
+
+    public customerController(CustomerServiceLogic customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.createCustomer(customer));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> getAllUsers() {
+        return ResponseEntity.ok(customerService.getAllCustomer());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerByID(@PathVariable long id) {
+        if (customerService.getCustomerByID(id) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(customerService.getCustomerByID(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(
+            @PathVariable long id,
+            @RequestBody Customer customer
+    ) {
+        if (customerService.updateCustomer(id, customer) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(customerService.updateCustomer(id, customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable long id) {
+        if (customerService.deleteCustomer(id)) {
+            return ResponseEntity.ok(" Customer Deleted");
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nothing found");
+    }
+
+}
