5da3598512535cb4e905179b606e2c939a929e37
Muhammad Tahir <150343129+Tahir-Dars@users.noreply.github.com>
2026-01-10 12:10:28 +0500

    add Customer entity with fields and constructors for bankCustomerAPI

--- src/main/java/com/bank/model/Customer.java
+++ src/main/java/com/bank/model/Customer.java
@@ -1,4 +1,67 @@
 package com.bank.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id
+    private long customerID;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String phoneNumber;
+
+    public Customer() {
+    }
+
+    public Customer(long customerID, String firstName, String email, String lastName, String phoneNumber) {
+        this.customerID = customerID;
+        this.firstName = firstName;
+        this.email = email;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(long customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
